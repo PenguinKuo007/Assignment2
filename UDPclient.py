5e51544d5766a964df2eacdd1dcbfcd922b7c672
Kuan-Lin Kuo <luke900228@gmail.com>
2023-03-19 01:44:19 -0400

    does not know when to terminate in client side

--- UDPclient.py
+++ UDPclient.py
@@ -8,47 +8,107 @@ y = 50  # packet size
 n = 3  # window size
 s = n * 2 + 1  # total number of sequence number
 seq = 0  # trace the current sequence number
-packet_list = []  # initialize an empty list of for packet
+packet_list = list(range(s))  # initialize an empty list of for packet
 ack_list = list(range(s))  # list for tracking current ack
 timeout = 3  # timeout value
 test = 0
-packet_list = []
+data_list = []
+create_list = list(range(s))
 start_timer = 0
 end_timer = 0
 
+for i in range(s):
+    create_list[i] = False
 for i in range(s):
     ack_list[i] = False
 
 
 # helper function to send filename to server
 def send_filename(test):
-    sock.settimeout(timeout)
     try:
-
+        packet_list.append('test1.txt')
         sock.sendto(b'test1.txt', server_address)
         data, server = sock.recvfrom(y)
 
         print(data)
-        print(test)
+
     except:
         test += 1
         print("didn't receive")
         send_filename(test)
 
 
-def send_packet(packet):
-    sock.settimeout(timeout)
+def send_packet(data_now, seq_base, seq_end):
+    current = seq_base
+
+    while current != seq_end:
+
+        if not create_list[current] and data_now != len(data_list):
+            if current < 10:
+                seq_str = b'0' + str(current).encode('utf-8')
+            else:
+                seq_str = str(current).encode('utf-8')
+            packet = seq_str + data_list[data_now]
+            packet_list[current] = packet
+            print(packet_list[current])
+            create_list[current] = True
+
+            data_now += 1
+        if not ack_list[current]:
+            sock.sendto(packet_list[current], server_address)
+
+        if current == s - 1:
+            current = 0
+        else:
+            current += 1
+
+    if data_now == len(data_list):
+        receive_data(data_now, seq_base, seq_end, True, current - 1)
+    else:
+        receive_data(data_now, seq_base, seq_end, False, 0)
+
+
+def receive_data(data_now, seq_base, seq_end, finish, last_seq):
     try:
-        sock.sendto(packet, server_address)
+
         data, server = sock.recvfrom(y)
-        return data
+
+        data = data.decode('utf-8')
+        ack = int(data)
+        ack_list[ack] = True
+
+        if ack == seq_base:
+
+            while ack_list[seq_base]:
+
+                ack_list[seq_base] = False
+                create_list[seq_base] = False
+
+                if seq_base == s - 1:
+                    seq_base = 0
+                else:
+                    seq_base += 1
+
+                if data_now != len(data_list):
+                    if seq_end == s - 1:
+                        seq_end = 0
+                    else:
+                        seq_end += 1
+        receive_data(data_now, seq_base, seq_end, finish, last_seq)
+
+
     except:
-        print("packet didn't receive")
-        send_packet(packet)
 
+        print(finish)
+        print(last_seq)
+        print(seq_base)
+        if seq_base == last_seq and finish:
+            print('closing socket')
+            sock.close()
 
-def send_packet_test(packet):
-    sock.sendto(packet, server_address)
+        # here is the problem
+        if not ack_list[seq_base]:
+            send_packet(data_now, seq_base, seq_end)
 
 
 # Create a UDP socket
@@ -61,89 +121,29 @@ try:
     file = open('client_data/test1.txt', "rb")
     body = file.read()
     size = len(body)
-
-    send_filename(test)
-
     data_start = 0
     data_end = y - 2
+    data_now = 0
 
-    seq += 1
-
-    # testing the timeout
-    for i in range(n):
-        if seq < 10:
-            seq_str = b'0' + str(seq).encode('utf-8')
-        else:
-            seq_str = str(seq).encode('utf-8')
-        packet = seq_str + body[data_start:data_end]
-        packet_list.append(packet)
-        print(packet_list[i])
+    seq_base = 0
+    seq_end = seq_base + n
+    packet_amount = 0
+    while data_end < size + y - 2:
+        data_list.append(body[data_start:data_end])
         data_start = data_end
         data_end = data_end + y - 2
-        if seq == s:
-            seq = 0
-        else:
-            seq += 1
-        size += 2
-        if (ack_list[i] == False):
-            send_packet_test(packet)
-            if i == 0:
-                end_timer = time.time()
-            else:
-
-                for x in range(i):
-                    timer = time.time()
-                    timer_list[i - 1] = timer_list[i - 1] + end_timer - start_timer
-                start_timer = time.time()
-
+        packet_amount += 1
     sock.settimeout(timeout)
-    while True:
-        try:
-            data, server = sock.recvfrom(y)
-            end_timer = time.time()
-            for i in range(n + 1):
-                timer_list[i] = timer_list + end_timer - start_timer
-            data = data.decode('utf-8')
-            ack = int(data)
-            ack_list[ack] = True
-        except:
-            print('bruh')
-            break
+    send_filename(test)
+    seq_base += 1
+    seq_end += 1
+    print(len(data_list))
+    data_now = send_packet(data_now, seq_base, seq_end)
 
-    for i in range(n + 1):
+    # maybe send_packet call receive_data at the end and receive_data all send_packet for the next packet?
+
+    for i in range(1, n + 1):
         print(ack_list[i])
-
-    for i in range(n + 1):
-        print(timer_list[i])
-
-    while data_end <= size:
-        if seq < 10:
-            seq_str = b'0' + str(seq).encode('utf-8')
-        else:
-            seq_str = str(seq).encode('utf-8')
-        packet = seq_str + body[data_start:data_end]
-        data_start = data_end
-        data_end = data_end + y - 2
-        if seq == s:
-            seq = 0
-        else:
-            seq += 1
-        size += 2
-        ack = send_packet(packet)
-
-        print(ack)
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- UDPserver.py
+++ UDPserver.py
@@ -7,8 +7,15 @@ import random
 y = 50  # packet size
 n = 3  # window size
 s = n * 2 + 1  # total number of sequence number
-current = 0  # trace the current sequence number
+
 filename_get = False  # flag to check if filename has successfully receive
+
+seq_base = 0
+seq_end = seq_base + n
+buffer = list(range(s))
+buf_check = list(range(s))
+for i in range(s):
+    buf_check[i] = False
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -31,22 +38,48 @@ while True:
             print(filename)
             file = open("server_data/" + filename, "wb")
 
-            ack = str(current).encode('utf-8')
+            ack = str(0).encode('utf-8')
             sock.sendto(ack, address)
-            current += 1
             filename_get = True
+            seq_base += 1
+            seq_end += 1
     else:
         data, address = sock.recvfrom(y)
-        print(data)
+        # print(data)
         loss = random.randint(1, 10)
-        if loss >= 1:
+        if loss >= 5:
             seq = data[0:2].decode('utf-8')
             seq = int(seq)
-            seq = str(seq)
+            seq_str = str(seq)
 
-            ack = seq.encode('utf-8')
+            ack = seq_str.encode('utf-8')
+
             body = data[2:]
-            file.write(body)
+            if seq != seq_base:
+                buffer[seq] = body
+                buf_check[seq] = True
+
+            else:
+
+                buffer[seq] = body
+                temp_end = seq_end
+                buf_check[seq] = True
+                while (seq_base != temp_end) and buf_check[seq_base]:
+                    file.write(buffer[seq_base])
+                    buf_check[seq_base] = False
+                    if seq_base == s - 1:
+                        seq_base = 0
+                    else:
+                        seq_base += 1
+                    if seq_end == s - 1:
+                        seq_end = 0
+                    else:
+                        seq_end += 1
+
+            print(ack)
             sock.sendto(ack, address)
-        else:
-            print('packet lost!')
+
+        # else:
+        # print('packet lost!')
+
+# TODO: after receiving all the packets, close the file, and set filename_get to false
