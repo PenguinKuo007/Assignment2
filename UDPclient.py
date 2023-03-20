755210746fa2a67f49d2137052b0fa056dafb5f9
Kuan-Lin Kuo <luke900228@gmail.com>
2023-03-20 19:49:17 -0400

    debug

--- UDPclient.py
+++ UDPclient.py
@@ -4,21 +4,21 @@ import time
 import random
 
 # Global variable
-hostname = 'localhost'
-filename = 'test2.txt'
+hostname = '142.1.46.51'
+filename = 'test1.txt'
 y = 50  # packet size
 n = 3  # window size
 s = n * 2 + 1  # total number of sequence number
 seq = 0  # trace the current sequence number
-packet_list = ['']*s  # initialize an empty list of for packet
+packet_list = [''] * s  # initialize an empty list of for packet
 ack_list = list(range(s))  # list for tracking current ack
-timeout = 3  # timeout value
+timeout = 1  # timeout value
 data_list = []
 create_list = list(range(s))
 start_timer = 0
 end_timer = 0
 max_rtt = 0
-min_rtt = 2**31 -1 
+min_rtt = 2 ** 31 - 1
 rtt_count = 0
 total_rtt = 0
 lost_packets = 0
@@ -26,11 +26,12 @@ total_packets = 0
 total_bytes = 0
 effective_bytes = 0
 timer_list = list(range(s))
-last_seq = -1
-
+last_seq = 100
+packet_sent = list(range(s))
 for i in range(s):
     ack_list[i] = False
     create_list[i] = False
+    packet_sent[i] = False
     timer_list[i] = 0
 
 
@@ -56,8 +57,10 @@ def send_filename():
 def send_packet(data_now, seq_base, seq_end, finish, last_seq):
     global total_packets, effective_bytes, total_bytes
     current = seq_base
-
-    while current != seq_end:
+    temp_end = seq_end
+    if finish:
+        temp_end = last_seq + 1
+    while current != temp_end and data_now <= len(data_list) + 1:
 
         if not create_list[current] and data_now != len(data_list):
             if current < 10:
@@ -71,11 +74,13 @@ def send_packet(data_now, seq_base, seq_end, finish, last_seq):
 
             data_now += 1
             if data_now == len(data_list):
+                print('here')
                 last_seq = current
-        if data_now == len(data_list) and not finish:
-            seq_end = current + 1
-        if not ack_list[current]:
+                return [True, last_seq, data_now]
+
+        if not ack_list[current] and not packet_sent[current]:
             sock.sendto(packet_list[current], server_address)
+            packet_sent[current] = True
             log.write(b'Sender: sent PKT' + str(current).encode('utf-8') + b'\n')
             timer_list[current] = time.time()
             total_packets = total_packets + 1
@@ -88,13 +93,15 @@ def send_packet(data_now, seq_base, seq_end, finish, last_seq):
             current += 1
 
     if data_now == len(data_list):
-        receive_data(data_now, seq_base, seq_end, True, last_seq)
+
+        return [True, last_seq, data_now]
     else:
-        receive_data(data_now, seq_base, seq_end, False, last_seq)
+        return [False, last_seq, data_now]
 
 
 def receive_data(data_now, seq_base, seq_end, finish, last_seq):
     global max_rtt, min_rtt, total_rtt, rtt_count, lost_packets, effective_bytes
+    complete = False
     try:
         data, server = sock.recvfrom(y)
         log.write(b'Sender: received ACK' + data + b'\n')
@@ -106,7 +113,7 @@ def receive_data(data_now, seq_base, seq_end, finish, last_seq):
         print("Start Time: " + str(timer_list[ack])[:13] + " sec")
         received = time.time()
         print("End Time: " + str(received)[:13] + " sec")
-        rtt = received - timer_list[ack] 
+        rtt = received - timer_list[ack]
         print("RTT: " + str(rtt)[:6] + " sec\n")
 
         if max_rtt < rtt:
@@ -115,16 +122,23 @@ def receive_data(data_now, seq_base, seq_end, finish, last_seq):
             min_rtt = rtt
         total_rtt = total_rtt + rtt
         rtt_count = rtt_count + 1
-        
+
         if ack == seq_base and finish and seq_base == last_seq:
+
             # print(seq_end)
             pass
         else:
             if ack == seq_base:
+
                 while ack_list[seq_base]:
 
+                    if finish and seq_base == last_seq and ack_list[seq_base]:
+                        complete = True
+                        # print(seq_end)
+                        break
                     ack_list[seq_base] = False
                     create_list[seq_base] = False
+                    packet_sent[seq_base] = False
 
                     if seq_base == s - 1:
                         seq_base = 0
@@ -136,7 +150,14 @@ def receive_data(data_now, seq_base, seq_end, finish, last_seq):
                             seq_end = 0
                         else:
                             seq_end += 1
-            receive_data(data_now, seq_base, seq_end, finish, last_seq)
+                if complete:
+                    pass
+                else:
+
+                    finish = send_packet(data_now, seq_base, seq_end, False, last_seq)
+                    receive_data(finish[2], seq_base, seq_end, finish[0], finish[1])
+            else:
+                receive_data(data_now, seq_base, seq_end, finish, last_seq)
 
 
     except Exception as e:
@@ -146,7 +167,12 @@ def receive_data(data_now, seq_base, seq_end, finish, last_seq):
         lost_packets = lost_packets + 1
         effective_bytes = effective_bytes - len(packet_list[seq_base])
         if not ack_list[seq_base]:
-            send_packet(data_now, seq_base, seq_end, finish, last_seq)
+            packet_sent[seq_base] = False
+            finish = send_packet(data_now, seq_base, seq_end, finish, last_seq)
+            receive_data(finish[2], seq_base, seq_end, finish[0], finish[1])
+        else:
+            receive_data(data_now, seq_base, seq_end, finish, last_seq)
+
 
 log = open('client_data/client_log', "wb")
 
@@ -176,7 +202,8 @@ try:
     seq_base += 1
     seq_end += 1
     # print(len(data_list))
-    send_packet(data_now, seq_base, seq_end, False, last_seq)
+    finish = send_packet(data_now, seq_base, seq_end, False, last_seq)
+    receive_data(finish[2], seq_base, seq_end, finish[0], finish[1])
 
 
 
@@ -185,11 +212,11 @@ finally:
     sock.sendto(b'Finished', server_address)
     sock.close()
     log.write(b'Sender: file transfer completed\n')
-    print("Maximum RTT: " + str(max_rtt*1000)[:4] + " msec")
-    print("Minimum RTT: " + str(min_rtt*1000)[:4]+ " msec")
-    print("Average RTT: " + str(total_rtt*1000/rtt_count)[:4]+ " msec")
+    print("Maximum RTT: " + str(max_rtt * 1000)[:4] + " msec")
+    print("Minimum RTT: " + str(min_rtt * 1000)[:4] + " msec")
+    print("Average RTT: " + str(total_rtt * 1000 / rtt_count)[:4] + " msec")
     # print(lost_packets, total_packets)
-    print("Packet loss rate: " + str(lost_packets*100/total_packets)[:5] + "%")
+    print("Packet loss rate: " + str(lost_packets * 100 / total_packets)[:5] + "%")
     log.write(b'Sender: number of effective bytes sent: ' + str(effective_bytes).encode('utf-8') + b' bytes\n')
     log.write(b'Sender: number of packets sent: ' + str(total_packets).encode('utf-8') + b' packets\n')
     log.write(b'Sender: number of bytes sent: ' + str(total_bytes).encode('utf-8') + b' bytes\n')
--- UDPserver.py
+++ UDPserver.py
@@ -18,11 +18,12 @@ total_bytes = 0
 for i in range(s):
     buf_check[i] = False
 
+test = 0
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to the port
-server_address = ('localhost', 10000)
+server_address = ('142.1.46.51', 10000)
 
 sock.bind(server_address)
 
@@ -34,7 +35,6 @@ while True:
 
     data, address = sock.recvfrom(y)
     total_bytes = total_bytes + len(data)
-    
 
     if not filename_get:
 
@@ -42,7 +42,7 @@ while True:
         loss = random.randint(1, 10)
 
         # if loss >= 3:
-        if True:
+        if loss >= 5:
             filename = data.decode('utf-8')
             print("Filename successfully received")
             file = open("server_data/" + filename, "wb")
@@ -55,7 +55,7 @@ while True:
             seq_base += 1
             seq_end += 1
     else:
-        
+
         if data.decode('utf-8') == 'Finished':
             filename_get = False
             file.close
@@ -68,40 +68,41 @@ while True:
             log.write(b'Receiver: received PKT' + str(int(data[0:2])).encode('utf-8') + b'\n')
             loss = random.randint(1, 10)
 
-            # if loss >= 3:
-            if True:   
-                seq = data[0:2].decode('utf-8')
-                seq = int(seq)
-                seq_str = str(seq)
+            if loss >= 5:
+                if True:
+                    test += 1
+                    seq = data[0:2].decode('utf-8')
+                    seq = int(seq)
+                    seq_str = str(seq)
 
-                print("Receieved: ACK" + str(seq) + ", Expected: ACK"+str(seq_base))
-                ack = seq_str.encode('utf-8')
-                
-                body = data[2:]
-                if seq != seq_base:
-                    buffer[seq] = body
-                    buf_check[seq] = True
+                    print("Receieved: ACK" + str(seq) + ", Expected: ACK" + str(seq_base))
+                    ack = seq_str.encode('utf-8')
 
-                else:
+                    body = data[2:]
+                    if seq != seq_base:
+                        buffer[seq] = body
+                        buf_check[seq] = True
 
-                    buffer[seq] = body
-                    temp_end = seq_end
-                    buf_check[seq] = True
-                    while (seq_base != temp_end) and buf_check[seq_base]:
-                        file.write(buffer[seq_base])
-                        buf_check[seq_base] = False
-                        if seq_base == s - 1:
-                            seq_base = 0
-                        else:
-                            seq_base += 1
-                        if seq_end == s - 1:
-                            seq_end = 0
-                        else:
-                            seq_end += 1
+                    else:
 
-                sock.sendto(ack, address)
-                log.write(b'Receiver: sent an ACK' + ack + b'\n')
-                
+                        buffer[seq] = body
+                        temp_end = seq_end
+                        buf_check[seq] = True
+                        while (seq_base != temp_end) and buf_check[seq_base]:
+                            file.write(buffer[seq_base])
+                            buf_check[seq_base] = False
+                            if seq_base == s - 1:
+                                seq_base = 0
+                            else:
+                                seq_base += 1
+                            if seq_end == s - 1:
+                                seq_end = 0
+                            else:
+                                seq_end += 1
+
+                    sock.sendto(ack, address)
+                    log.write(b'Receiver: sent an ACK' + ack + b'\n')
+
 
 
 
